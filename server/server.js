fddc6e5bd1fee222f7516731f938c4f93c72ded2
MUHAMMAD AHSAN KHAN <163602037+Muhammadahsankhn@users.noreply.github.com>
2025-12-08 19:43:53 +0500

    Set default PORT to 5000 if not specified

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./src/app');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/healthMate ')
