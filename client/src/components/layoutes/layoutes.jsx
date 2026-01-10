66378dbefc7e348123c412e5c2e5c40c036da1bc
Ahsan <ahsankhn0987@gmail.com>
2026-01-11 03:27:40 +0500

    mod

--- client/src/components/layoutes/layoutes.jsx
+++ client/src/components/layoutes/layoutes.jsx
@@ -1,16 +1,12 @@
 // src/layouts/Layout.jsx
 import React from "react";
-import Navbar from "../Navbar";
-import Footer from "../Footer";
 
 const Layout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-br from-green-50 via-white to-green-100">
-      <Navbar />
       <main className="flex-1 container mx-auto px-6 py-10">
         {children}
       </main>
-      <Footer />
     </div>
   );
 };
