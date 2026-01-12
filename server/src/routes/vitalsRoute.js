b389c60172a6a993f6ec228b36d5ab5cac6e1ee0
Ahsan <ahsankhn0987@gmail.com>
2026-01-13 01:35:24 +0500

    routes

--- server/src/routes/vitalsRoute.js
+++ server/src/routes/vitalsRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
-const { addVitals, getVitalsById, getVitalById } = require('../controllers/vitalControllers');
+const { addVitals, getUserVitals, getVitalById } = require('../controllers/vitalControllers');
 
 if(!addVitals) console.log("singular vital not found")
-if(!getVitalsById) console.log("singular vital not found")
+if(!getUserVitals) console.log("singular vital not found")
 if(!getVitalById) console.log("singular vital not found")
 router.post('/addVitals', protect, addVitals);
-router.get("/allVitals", protect, getVitalsById);
+router.get("/allVitals", protect, getUserVitals);
 router.get('/:id', protect, getVitalById);
 
 
