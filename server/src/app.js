472f5897bbf229e5f4926a2180469c31410edeca
Ahsan <ahsankhn0987@gmail.com>
2026-01-17 01:25:33 +0500

    Fix CORS for Vercel

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: '*', 
     credentials: true, // Allow cookies to be sent
 };
 const app = express();
